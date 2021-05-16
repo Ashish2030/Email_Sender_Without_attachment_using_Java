cd0064f97eb03de6566898eac250076bf7b9e436
Ashish2030 <Kumarashish79924@gmail.com>
2021-05-16 11:46:58 +0530

    𝐅𝐢𝐧𝐚𝐥 𝐏𝐫𝐨𝐣𝐞𝐜𝐭

---
+++ src/main/java/com/ashish/Email_Sender_Using_Java/App.java
@@ -0,0 +1,85 @@
+package com.ashish.Email_Sender_Using_Java;
+import java.util.*;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class App 
+{
+	
+	public static void main( String[] args )
+    {
+		System.out.println("preparing to send message ...");
+		String message = "Happy Coding  ";
+		String subject = "Checking my Code";
+		String to = "rs8657511@gmail.com";
+		String from = "Kumarashish79924@gmail.com";
+	
+		sendEmail(message,subject,to,from);
+	}
+private static void sendEmail(String message, String subject, String to, String from) {
+		
+		//Variable for gmail
+		String host="smtp.gmail.com";
+		
+		//get the system properties
+		Properties properties = System.getProperties();
+		System.out.println("PROPERTIES "+properties);
+		
+		//setting important information to properties object
+		
+		//host set
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port","465");
+		properties.put("mail.smtp.ssl.enable","true");
+		properties.put("mail.smtp.auth","true");
+		
+		//Step 1: to get the session object..
+		Session session=Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				// TODO Auto-generated method stub
+				return new PasswordAuthentication("kumarashish79924@gmail.com","*******");
+			}
+		});
+		
+		session.setDebug(true);
+		
+		//Step 2 : compose the message [text,multi media]
+		MimeMessage m = new MimeMessage(session);
+		
+		try {
+		
+		//from email
+		m.setFrom(from);
+		
+		//adding recipient to message
+		m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		
+		//adding subject to message
+		m.setSubject(subject);
+	
+		
+		//adding text to message
+		m.setText(message);
+		
+		//send 
+		
+		//Step 3 : send the message using Transport class
+		Transport.send(m);
+		
+		System.out.println("Sent success...................");
+		
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+	}
+
+}
